f709a4ec4425827309e31568255dd1e03ab1d376
NoyannOzmen <noyann.ozmen@oclock.school>
2024-06-03 17:06:50 +0200

    Tout est OK (en desktop-first, je sais) sauf : MediaQueries / Limitation du catalogue à 3 items avant clic du b > outon / A11y + CSS à fignoler

--- app/dataMapper.js
+++ app/dataMapper.js
@@ -7,17 +7,17 @@ const dataMapper = {
     return result.rows;
   },
   getSomeProducts : async () => {
-    const sql = `SELECT * FROM coffees LIMIT 3 OFFSET 13`;
+    const sql = `SELECT * FROM coffees ORDER BY name LIMIT 3 OFFSET 13`;
     const result = await client.query(sql);
     return result.rows;
   },
   getProductbyId : async (prodId) => {
-    const sql = `SELECT * FROM coffees WHERE id = ${prodId}`;
+    const sql = `SELECT * FROM coffees WHERE id = ${prodId} ORDER BY name`;
     const result = await client.query(sql);
     return result.rows[0];
   },
   getProductbyCategory : async (prodCat) => {
-    const sql = `SELECT * FROM coffees WHERE category = '${prodCat}'`;
+    const sql = `SELECT * FROM coffees WHERE category = '${prodCat}' ORDER By name`;
     const result = await client.query(sql);
     return result.rows;
   },
--- index.js
+++ index.js
@@ -11,12 +11,11 @@ const app = express();
 app.set("views", "./app/views");
 app.set("view engine", "ejs");
 
-app.use("/public", express.static('integration'));
+app.use("/integration", express.static('integration'));
 
 app.use(router);
 
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
+app.use((req, res) => {
   res.status(404).render("404");
 });
 
